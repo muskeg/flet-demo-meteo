45d82e6e4706c70de1adb40439203ae210b084ef
raph <raph@airbook.local>
2024-11-09 10:22:12 -0500

    test ECweather

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import flet as ft
 from env_canada import ECWeather
 import meteo.conditions as conditions
@@ -7,7 +8,9 @@ import meteo.previsions as previsions
 def main(page: ft.Page):
     page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
 
-    test_meteo = ECWeather(coordinates=(45.630001, -73.519997))
-
+    test_meteo = ECWeather(coordinates=(45.50884, -73.58781))
+    asyncio.run(test_meteo.update())
+    print(test_meteo.station_id)
+    print(test_meteo.hourly_forecasts)
 
 ft.app(main)
