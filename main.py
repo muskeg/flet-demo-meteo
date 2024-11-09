bdc844d288922bf46ac86e38d3a49e02572be68b
raph <raph@airbook.local>
2024-11-09 09:32:06 -0500

    ajout envcanada

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import flet as ft
+from env_canada import ECWeather
+import meteo.conditions as conditions
+import meteo.previsions as previsions
 
 
 def main(page: ft.Page):
     page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+
+    test_meteo = ECWeather(coordinates=(45.630001, -73.519997))
 
 
 ft.app(main)
